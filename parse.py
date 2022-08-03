7d711149c701a11518c59e573c144a942c623484
leo3friedman <73138942+leo3friedman@users.noreply.github.com>
2022-08-03 11:14:06 -0700

    convert hard code to functions

--- parse.py
+++ parse.py
@@ -2,22 +2,32 @@ import pandas as pd
 # hide warning message (default='warn') TODO: read why this is occurring
 pd.options.mode.chained_assignment = None
 # convert html file into list of dataframes
-dfs = pd.read_html("html-data/anth.html")
+dataframe_list = pd.read_html("html-data/anth.html")
 # first dataframe in list
-df = dfs[0]
-# only rows with specific course
-df_anar_116 = df[df['Course'].str.contains('ANAR 116')]
+dataframe = dataframe_list[0]
 # remove columns we don't care about
-df_anar_116.drop(['Term', 'Instructor', 'Enroll', 'Rcmnd Class', 'Rcmnd Instr', 'Avg Grade Received', 'Avg Grade Expected'], axis=1, inplace=True)
-# simplify course name
-df_anar_116['Course'] = 'ANAR 116'
+dataframe.drop(['Term', 'Instructor', 'Enroll', 'Rcmnd Class', 'Rcmnd Instr', 'Avg Grade Received', 'Avg Grade Expected'], axis=1, inplace=True)
 
-total_evals = df_anar_116['Evals Made'].sum()
-study_hrs_avg = round(((df_anar_116['Evals Made'] * df_anar_116['Study Hrs/wk']).sum() / total_evals), 2)
-print('\nANAR 116 Avg Study Hours :\n', study_hrs_avg)
+
+def locate_course(df, course):
+    # only rows with specific course
+    df_course = df[df['Course'].str.contains(course)]
+    # simplify course name
+    df_course['Course'] = course
+    return df_course
+
+
+def calc_avg_study_hours(df):
+    total_evaluations = df['Evals Made'].sum()
+    study_hrs_avg = round(((df['Evals Made'] * df['Study Hrs/wk']).sum() / total_evaluations), 2)
+    return study_hrs_avg
+
+
+anar_116_courses = locate_course(dataframe, 'ANAR 116')
+anar_116_avg_study_hours = calc_avg_study_hours(anar_116_courses)
 
 # Displays entire dataframe
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print('\nANAR 116 dataframe :\n', df_anar_116)
+    print('\nANAR 116 dataframe :\n', anar_116_courses)
+    print('\nANAR 116 avg study hours :\n', anar_116_avg_study_hours)
 
-    
